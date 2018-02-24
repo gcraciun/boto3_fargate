4d92947ed92a2ba124595917c65960170283cb10
gcraciun <en.gcraciun@gmail.com>
2018-02-24 14:02:34 +0000

    adding proper error check

--- fargate.py
+++ fargate.py
@@ -3,6 +3,7 @@
 import boto3
 import sys
 import json
+from botocore.exceptions import ClientError
 
 try:
     inputf = sys.argv[1]
@@ -21,7 +22,7 @@ def read_data(ifile):
 
 task_data = read_data(inputf)
 
-print task_data
+#print task_data
 
 # If it does not exist create Cloudwatch LogGroup.
 # - Move from hardcoding to variable after testing.
@@ -29,14 +30,13 @@ cw = boto3.client('logs')
 try:
     loggroups = cw.describe_log_groups(logGroupNamePrefix='Fargate-Test')
 #    loggroups = cw.describe_log_groups()
-except:
-    print "Error getting log groups"
+except ClientError as e:
+    print e
     exit(1)
 
 LGs = [ x["logGroupName"] for x in loggroups["logGroups"]]
 
 lg_found = False # 2nd grade programming
-
 if len(LGs) > 0:
     for temp in LGs:
         if temp == 'Fargate-Test':
@@ -48,20 +48,17 @@ if not lg_found:
     print "proceeding with creation"
     try:
         newlg = cw.create_log_group(logGroupName='Fargate-Test')
-    except:
-        print "Error creating log group"
+    except ClientError as e:
+        print e
         exit(1)
 
-#print "\n\n\nExiting without running everything below"
-#exit(0)
-
-# Get there first
 try:
     ec2 = boto3.client('ec2')
     vpc = ec2.create_vpc(CidrBlock='172.21.0.0/16')
-except:
-    print "Error creating VPC, send me to http://botocore.readthedocs.io/en/latest/client_upgrades.html#error-handling"
+except ClientError as e:
+    print e
     exit(1)
+
 vpc_id = vpc["Vpc"]["VpcId"]
 print vpc_id
 
@@ -69,25 +66,24 @@ print vpc_id
 # Get available AZ's
 try:
     av_az = ec2.describe_availability_zones()
-    AZs = [x["ZoneName"] for x in av_az["AvailabilityZones"]]
-except:
-    print "Error getting AZ's"
+except ClientError as e:
+    print e
     exit(1)
 
+AZs = [x["ZoneName"] for x in av_az["AvailabilityZones"]]
 for x in AZs:
     print x
-# ADD, I need to push in alphabetical order A, B, C, etc
-AZs.reverse()
-
+AZs.reverse() # ADD, I need to push in alphabetical order A, B, C, etc
 
 try:
     print "Subnet 1 going"
     subnet1 = ec2.create_subnet(CidrBlock='172.21.1.0/24', VpcId=vpc_id, AvailabilityZone=AZs.pop())
     print "Subnet 2 going"
     subnet2 = ec2.create_subnet(CidrBlock='172.21.2.0/24', VpcId=vpc_id, AvailabilityZone=AZs.pop())
-except:
-    print "Error creating subnets"
+except ClientError as e:
+    print e
     exit(1)
+
 print subnet1["Subnet"]["SubnetId"]
 print subnet2["Subnet"]["SubnetId"]
 
@@ -95,5 +91,6 @@ print subnet2["Subnet"]["SubnetId"]
 ecs = boto3.client('ecs')
 try:
     ecs.create_cluster(clusterName='GC-Test')
-except:
-    print "Error creating cluster"
+except ClientError as e:
+    print e
+    exit(1)
